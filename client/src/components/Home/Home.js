e50044bc3bf512fb66b30b42e738b856d57e6aca
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2022-09-16 00:17:36 +0200

    Cosmetic changes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -19,12 +19,12 @@ const Home = () => {
 
     return (
         <Grow in>
-            <Container style={{marginTop: "80px"}}>
+            <Container maxWidth="md" style={{marginTop: "80px"}}>
                 <Grid container className={style.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={8}>
+                    <Grid item xs={12} sm={6} md={8}>
                         <Posts setCurrentId={setCurrentId}/>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={6} md={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                     </Grid>
                 </Grid>
