989da11dda73073cf3bfc83ea2640be87a11fbb1
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2022-09-29 00:06:39 +0200

    Preparations for search module

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
+import { useLocation, useNavigate } from "react-router-dom";
+import ChipInput from 'material-ui-chip-input';
 
 import useStyles from './styles'
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
     const style = useStyles();
     const dispatch = useDispatch();
     const [currentId, setCurrentId] = useState(0);
+    const navigate = useNavigate();
+    const query = useQuery();
+    const searchQuery = query.get('searchQuery');
 
     useEffect(() => {
         dispatch(getPosts());
@@ -21,10 +30,20 @@ const Home = () => {
         <Grow in>
             <Container maxWidth="md" style={{marginTop: "80px"}}>
                 <Grid container className={style.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={6} md={8}>
+                    <Grid item xs={12} sm={6} md={9}>
                         <Posts setCurrentId={setCurrentId}/>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar className={style.searchBar} position="static" color="inherit">
+                            <TextField
+                                name="search"
+                                variant="outlined"
+                                label="Search"
+                                fullWidth
+                                value="test"
+                                onChange={() => {}}
+                            />
+                        </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                     </Grid>
                 </Grid>
